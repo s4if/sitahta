bff4a4d48a7c112ef6532246a221d08362daa835
s4if <sangpejuang32@gmail.com>
2015-02-03 12:55:54 +0700

    at least ok

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -60,7 +60,7 @@ $autoload['packages'] = array();
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
 
-$autoload['libraries'] = array('database','session');
+$autoload['libraries'] = array('database');
 
 
 /*
@@ -76,7 +76,7 @@ $autoload['libraries'] = array('database','session');
 |	$autoload['drivers'] = array('cache');
 */
 
-$autoload['drivers'] = array();
+$autoload['drivers'] = array('');
 
 
 /*
